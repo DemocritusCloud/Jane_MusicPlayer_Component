e4bf340ec50dc2bca66d4f61696aaf34c7d388ae
Arshavin86 <sevost.andrey@gmail.com>
2019-01-09 16:36:45 -0800

    Created schema and table (relation)

--- schema.sql
+++ schema.sql
@@ -1,3 +1,14 @@
-DROP DATABASE IF EXISTS soundcloud;
-CREATE DATABASE soundcloud;
-USE soundcloud;
+
+ \connect soundcloud;
+ CREATE SCHEMA IF NOT EXISTS testSong AUTHORIZATION user1;
+    CREATE TABLE songs (
+        album text, 
+        artist text, 
+        duration integer,
+        id integer,
+        released date,
+        title text,
+        wave text,
+        image text,
+        song_url text
+    );
