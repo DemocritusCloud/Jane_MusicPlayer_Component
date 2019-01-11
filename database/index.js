66522d70bc10d432da4fbf2bbbd86fe9071a2643
Arshavin86 <sevost.andrey@gmail.com>
2019-01-10 21:25:43 -0800

    Seeded with 10m for 5:48

--- database/index.js
+++ database/index.js
@@ -1,33 +1,89 @@
-const Sequelize = require('sequelize');
 
-const db = new Sequelize('soundcloud', 'user1', 'hallinthewall', {
-  host: 'localhost',
-  dialect: 'postgres',
+const pgp = require('pg-promise')({});
+
+const playlist = require('./seed.js');
+
+const databaseConfig = {
+  "host": "localhost",
+  "port": 5432,
+  "database": "soundcloud",
+  "user": "andrei",
+  "password": "privet",
+};
+
+// console.log(playlist);
+
+const db = pgp(databaseConfig);
+
+function millisToMinutesAndSeconds(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
+
+function getNextData(t, pageIndex) {
+  let data = null;
+  if (pageIndex < 1000) {
+    data = [];
+    for (let i = 0; i < 10000; i++) {
+      const idx = pageIndex * 10000 + i;
+      console.log(idx);
+      data.push({
+        id: idx,
+        album: playlist.songArray[idx].album,
+        artist: playlist.songArray[idx].artist,
+        duration: playlist.songArray[idx].duration,
+        released: playlist.songArray[idx].released,
+        title: playlist.songArray[idx].title,
+        image: playlist.songArray[idx].image,
+        song_url: playlist.songArray[idx].song_url,
+      });
+    }
+  }
+  return Promise.resolve(data);
+}
+
+const cs = new pgp.helpers.ColumnSet([
+  'id',
+  'album',
+  'artist', 
+  'duration',
+  'released',
+  'title',
+  'image',
+  'song_url',
+], {table: 'songs'});
+
+db.connect()
+  .then(obj => {
+    console.log('connected to database');
+      obj.done(); // success, release the connection;
+  })
+  .catch(error => {
+      console.log('ERROR:', error.message || error);
 });
 
-db.authenticate()
-  .then(() => { console.log('connected to database'); })
-  .catch((error) => { console.error(error); });
+db.tx('massive-insert', t => {
+  return t.sequence(index => {
+    // if (index === 0) {
+    //   console.log(index)
+    // } 
+    return getNextData(t, index)
+    .then(data => {
+      if (data) {
+          const insert = pgp.helpers.insert(data, cs);
+          return t.none(insert);
+      }
+    });
+  });
+})
+.then(data => {
+  // COMMIT has been executed
+  console.log('Total batches:', data.total, ', Duration:', millisToMinutesAndSeconds(data.duration));
+})
+.catch(error => {
+  // ROLLBACK has been executed
+  console.log(error);
+});
 
-const songs = db.define(
-  'songs',
-  {
-    title: { type: Sequelize.STRING },
-    artist: { type: Sequelize.STRING },
-    album: { type: Sequelize.STRING },
-    released: { type: Sequelize.DATE },
-    duration: { type: Sequelize.INTEGER },
-    wave: { type: Sequelize.TEXT },
-    image: { type: Sequelize.STRING },
-    song_url: { type: Sequelize.STRING },
-  },
-  {
-    timestamps: false,
-  },
-);
 
-db.sync()
-  .then(() => { console.log('synced with postgres database'); })
-  .catch((error) => { console.error(error); });
-
-module.exports = { db, songs };
--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,4 @@
 const faker = require('faker');
-const db = require('./index');
 
 // alternate between 5 songs to be played for each data, but each data will have diff. wave form
 const randomSongs = [
@@ -10,26 +9,34 @@ const randomSongs = [
   'https://s3-us-west-1.amazonaws.com/democrituscloud/bensound-jazzyfrenchy.mp3',
 ];
 
-// create a function to generate random wave data in string form, later to be split into an array
-const waveGenerator = () => {
-  let waves = '';
-  for (let i = 0; i <= 240; i += 1) {
-    const wave = Math.floor((Math.random() + 1) * 35);
-    waves += `${wave},`;
-  }
-  return waves;
-};
+  let songArray = [];
 
-// create an entry for each item in the db
-for (let i = 0; i <= 10000; i += 1) {
-  db.songs.create({
-    title: faker.lorem.word(),
-    artist: faker.name.findName(),
-    album: faker.lorem.word(),
-    released: faker.date.past(),
-    duration: faker.random.number({ min: 100, max: 300 }),
-    wave: waveGenerator(),
-    image: faker.image.abstract(),
-    song_url: randomSongs[Math.floor(Math.random() * 5)],
-  });
-}
+  // create a function to generate random wave data in string form, later to be split into an array
+// const waveGenerator = () => {
+//   let waves = '';
+//   for (let i = 0; i <= 240; i += 1) {
+//     const wave = Math.floor((Math.random() + 1) * 35);
+//     waves += `${wave},`;
+//   }
+//   return waves;
+// };
+
+  for (let i = 1; i <= 10000000; i += 1) {
+    let obj1 = {
+      title: faker.lorem.word(),
+      artist: faker.name.findName(),
+      album: faker.lorem.word(),
+      released: faker.date.past(), // JavaScript heap out of memory on this step
+      duration: faker.random.number({ min: 100, max: 300 }),
+      // wave: waveGenerator(),
+      image: faker.image.abstract(),
+      song_url: randomSongs[Math.floor(Math.random() * 5)],
+    };
+    songArray.push(obj1);
+  }
+console.log(songArray.length);
+
+
+module.exports = { songArray };
+
+
